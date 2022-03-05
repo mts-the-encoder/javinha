0c8c7c555c07a1935d0062eb7cd090b9d8561cd3
zmtheus <matheus.nsantos@bandtec.com.br>
2022-03-05 17:08:39 -0300

    ex9

---
+++ bandtec/lista2/src/com/company/ContadorVariado.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class ContadorVariado {
+
+    public static void main(String[] args) {
+
+        double i = 0;
+        for (; i < 5; i += 0.15) {
+            System.out.printf("%.2f%n", i);
+        }
+    }
+}
