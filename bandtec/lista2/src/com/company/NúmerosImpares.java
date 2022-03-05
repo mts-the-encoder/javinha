8e329bfb5045812fb3ce0adb31eb5c96ed936152
zmtheus <matheus.nsantos@bandtec.com.br>
2022-03-05 02:06:45 -0300

    ex4

---
+++ bandtec/lista2/src/com/company/NúmerosImpares.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class NúmerosImpares {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 90; i++) {
+
+            if (i % 2 == 0) {
+                System.out.println("");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
