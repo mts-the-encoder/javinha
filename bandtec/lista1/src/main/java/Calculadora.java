5b493f2792e3f37c36b23db1c1de6447ce8b37ce
zmtheus <matheus.nsantos@bandtec.com.br>
2022-03-06 04:28:47 -0300

    ex5

---
+++ bandtec/lista1/src/main/java/Calculadora.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+
+        System.out.println("Digite um número \n");
+        Scanner leitorUm = new Scanner(System.in);
+        Integer digitoUm = leitorUm.nextInt();
+
+        System.out.println("Digite mais um número \n");
+        Scanner leitorDois = new Scanner(System.in);
+        Integer digitoDois = leitorDois.nextInt();
+
+        int soma = digitoUm + digitoDois;
+        System.out.printf("Resultado da soma: \n %d%n", soma);
+
+        int subtracao = digitoUm - digitoDois;
+        System.out.printf("Resultado da subtração: \n %d%n", subtracao);
+
+        int multiplicacao = digitoUm * digitoDois;
+        System.out.printf("Resultado da multiplicação: \n %d%n", multiplicacao);
+
+        double divisao = (double) digitoUm / digitoDois;
+        System.out.printf("Resultado da divisão: \n %.1f%n", divisao);
+
+    }
+}
